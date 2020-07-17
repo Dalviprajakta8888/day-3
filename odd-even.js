90880b1c10c7e78a812353e736687da2c4f5338d
Dalviprajakta8888 <68224980+Dalviprajakta8888@users.noreply.github.com>
2020-07-17 10:16:56 +0530

    Add files via upload

---
+++ odd-even.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"> 
+        content="width=device-width,intial-sacle=1.0"> 
+        <title>Day 3 assignment</title>
+    </head>     
+    <body>
+        <h1>odd even</h1>
+    <script>
+         let number =function(num1=0){
+             if(num1 %2==0){
+                 console.log(`the no ${num1} is even number`);
+             }
+             else{
+                 console.log(`the number ${num1}is odd number`);
+             }
+         }  
+        
+    </script>
+    </body>
+</html>
